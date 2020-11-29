ef1f9a9b77c58f845a7f5e1d845ee7470a3bb585
Daniel Gil <dgilalegre96@gmail.com>
2020-11-29 23:26:17 +0100

    Agregar vista de las películas

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Film
+from .serializers import FilmSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class FilmViewSet(viewsets.ModelViewSet):
+    queryset = Film.objects.all()
+    serializer_class = FilmSerializer
