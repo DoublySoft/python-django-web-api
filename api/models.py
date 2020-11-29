b8b7687177181cc670e060d4a8cad1a03a337a30
Daniel Gil <dgilalegre96@gmail.com>
2020-11-29 23:20:03 +0100

    Agregar modelo Film

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Film(models.Model):
+    title = models.CharField(max_length=150)
+    premiere = models.IntegerField(default=2000)
+    image = models.URLField(help_text="De imdb mismo")
+    summary = models.TextField(help_text="Descripción corta")
+
+    class Meta:
+        ordering = ['title']
